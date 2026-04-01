d941378a0e9c756a0b508fce443a7b6d370fc68d
Joseph Kerkhof <jkerkhof@actengage.com>
2026-04-01 09:47:23 -0500

    Add YAML LSP and treesitter support

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -603,6 +603,7 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         basedpyright = {}, -- Python
+        yamlls = {}, -- YAML
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -874,7 +875,7 @@ require('lazy').setup({
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      local parsers = { 'bash', 'c', 'diff', 'go', 'gomod', 'gosum', 'gowork', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      local parsers = { 'bash', 'c', 'diff', 'go', 'gomod', 'gosum', 'gowork', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'yaml' }
       require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
