f2109f430e964dcec990a51437bffe706b7280aa
Joseph Kerkhof <jkerkhof@actengage.com>
2026-04-01 14:18:36 -0500

    Move stylua from LSP servers to Mason ensure_installed
    stylua is a formatter, not a language server. It was being passed
    to vim.lsp.config/enable where it does nothing useful.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -610,8 +610,6 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
 
-        stylua = {}, -- Used to format Lua code
-
         -- Special Lua Config, as recommended by neovim help docs
         lua_ls = {
           on_init = function(client)
@@ -651,7 +649,7 @@ require('lazy').setup({
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        -- You can add other tools here that you want Mason to install
+        'stylua',
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
