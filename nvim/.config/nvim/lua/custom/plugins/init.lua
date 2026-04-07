5b8a5468bc055e3129563a8c74a03b610ab855ee
Joseph Kerkhof <jkerkhof@actengage.com>
2026-04-07 14:24:45 -0500

    Add lazygit keymap to toggleterm

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -20,6 +20,18 @@ return {
     },
     keys = {
       { '<leader>tt', desc = '[T]oggle [T]erminal' },
+      {
+        '<leader>lg',
+        function()
+          require('toggleterm.terminal').Terminal
+            :new({
+              cmd = 'lazygit',
+              direction = 'float',
+            })
+            :toggle()
+        end,
+        desc = '[L]azy[G]it',
+      },
     },
   },
 }
