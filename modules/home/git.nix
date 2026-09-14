5d053af986547a496399df5c939a0f8b5fdb6ca4
Joseph Kerkhof <joseph@kerkhof.dev>
2026-09-14 14:07:17 -0500

    fix(gpg): use on-demand agent on macOS

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,20 +1,31 @@
 {pkgs, ...}: {
-  programs.gpg.enable = true;
+  home = {
+    packages = [pkgs.pinentry_mac];
 
-  services.gpg-agent = {
-    enable = true;
-    enableZshIntegration = true;
-    pinentry.package = pkgs.pinentry_mac;
+    # Home Manager's Darwin service cannot pass launchd sockets to
+    # gpg-agent's supervised mode, so let GnuPG start the agent on demand.
+    file.".gnupg/gpg-agent.conf".text = ''
+      grab
+      pinentry-program ${pkgs.pinentry_mac}/bin/pinentry-mac
+    '';
   };
 
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
+  programs = {
+    gpg.enable = true;
 
-    settings = {
-      core.editor = "nvim";
-      push.autoSetupRemote = true;
-      user.useConfigOnly = true;
+    zsh.initContent = ''
+      export GPG_TTY=$TTY
+    '';
+
+    git = {
+      enable = true;
+      lfs.enable = true;
+
+      settings = {
+        core.editor = "nvim";
+        push.autoSetupRemote = true;
+        user.useConfigOnly = true;
+      };
     };
   };
 }
