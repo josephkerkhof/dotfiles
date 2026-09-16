e4ec87d4a8a572984d84a7a089612d5fc29a15ba
Joseph Kerkhof <joseph@kerkhof.dev>
2026-09-16 13:42:48 -0500

    feat: add dnsmasq

--- modules/darwin/hosts.nix
+++ modules/darwin/hosts.nix
@@ -1,6 +1,14 @@
 {inputs, ...}: {
   imports = [inputs.private-assets.darwinModules.homestar];
 
+  # Resolve every *.test name to loopback through DNS, as Herd's dnsmasq did.
+  # /etc/hosts is a symlink into /nix/store here, and Chrome's sandbox cannot
+  # read it, so local sites must come from DNS.
+  services.dnsmasq = {
+    enable = true;
+    addresses.test = "127.0.0.1";
+  };
+
   environment.etc.hosts.text = ''
     ##
     # Host Database
